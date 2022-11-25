f8858dc099e13e54491642f262d04e3e24cff2bc
NehaSharma-10 <shrmatwinkle246@gmail.com>
2022-11-25 19:45:00 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// const icon_up1 = document.getElementById('up-icon1');
+// const icon_down1 = document.getElementById('down-icon1');
+// const ans1 = document.getElementById('ans-text-1');
+// function icon1Click() {
+//     icon_down1.style.display = 'none';
+//     icon_up1.style.display = 'block';
+//     ans1.style.display = 'block';
+// }
